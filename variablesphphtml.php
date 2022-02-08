5ea976fa3b4133d78a9556f50f650a32c2523d16
Jucep <panditasty@gmail.com>
2022-02-08 14:59:58 -0500

    yo

---
+++ variablesphphtml.php
@@ -0,0 +1,11 @@
+<?php
+$d=rand(1,100);
+//echo "el valor aleatorio positivo es igual a:".$d;
+$d=rand(0,-100);
+//echo "el valor aleatorio negativo es igual a:".$d;
+/*Mod*/
+$anio_Actual=2022;
+$anio_Nacimiento=2003;
+$edad=$anio_Actual-$anio_Nacimiento;
+echo"edad".$edad;
+
